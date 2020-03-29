e9db6e631769cd3555cd2b678e216aa5969a34c3
shreyaschandra <61939562+shreyaschandra@users.noreply.github.com>
2020-03-29 11:05:43 +0530

    Add files via upload
    adding day 16 import classes and exceptional handling file.

---
+++ Testfile.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+class Test:
+    
+    def createname(self,name):
+        self.name=name
+        
+    def displayname(self):
+        print(self.name)
+        
+    def greet(self):
+        print(f"Hi, good to see you {self.name}")
+
+
+# In[ ]:
+
+
+
+
